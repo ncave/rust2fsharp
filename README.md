1c52cec74c09613668091922f1c599f6a6396b3f
ncave <777696+ncave@users.noreply.github.com>
2021-06-07 10:19:47 -0700

    Update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rust2fsharp
+Small tool to assist with porting Rust code to F#.
+Replaces some Rust patterns with F# equivalents.
--- src/Patterns.fs
+++ src/Patterns.fs
@@ -18,7 +18,7 @@ let replacePatterns text =
     |> replace (@"^(pub |crate )?use ", @"//open ")                 // replace "use " with "open"
     |> replace (@"^(pub |crate )?mod (.+);$", @"open $2")           // replace mod with open
 
-    |> replace (@"^( {4,})(pub )?(\w+ *:[^:].*),\s*\}$", @"$1$3 }") // replace struct fields 1
+    |> replace (@"^( {1,4})(pub )?(\w+ *:[^:].*),\s*\}$", @"$1$3 }") // replace struct fields 1
     |> replace (@"^( {1,4})(pub )?(\w+ *:[^:].*),$", @"$1$3")       // replace struct fields 2
     |> replace (@"^(pub )?struct (.+)\{$", @"type $2= {")           // replace struct with type
     |> replace (@"^( {1,4})(\w+)\(([^)\n]+)\),( *//.*)?$", @"$1| $2 of $3") // replace union cases 1
@@ -52,11 +52,12 @@ let replacePatterns text =
     |> replace (@"([^""])\bcrate ", @"$1")                          // replace "crate " with ""
 
     |> replace (@"^(\s*)where(\s*[^{]+),\s*\{", @"$1when$2 =")      // replace where's "{" with "when"
-    |> replace (@"(\bif .*)\{", @"$1then")                          // replace if's "{" with "then"
+    |> replace (@"(\bif .*?)\{(.*) \}", @"$1then$2")                // replace if's "{...}" with "then"
     |> replace (@"(\}\s*)?(else)\s*\{", @"$2")                      // replace "} else {" with "else"
-    |> replace (@"(\bmatch .*)\{", @"$1with")                       // replace match's "{" with "with"
-    |> replace (@"(\bfor .*)\{", @"$1do")                           // replace for's "{" with "do"
-    |> replace (@"(\while .*)\{", @"$1do")                          // replace while's "{" with "do"
+    |> replace (@"(\bif .*?)\{", @"$1then")                         // replace if's "{" with "then"
+    |> replace (@"(\bmatch .*?)\{", @"$1with")                      // replace match's "{" with "with"
+    |> replace (@"(\bfor .*?)\{", @"$1do")                          // replace for's "{" with "do"
+    |> replace (@"(\bwhile .*?)\{", @"$1do")                        // replace while's "{" with "do"
 
     |> replace (@"([^""])\bref ", @"$1")                            // replace "ref " with ""
     |> replace (@"([^""])\blet mut ", @"$1let mutable ")            // replace "mut" with "mutable"
@@ -70,19 +71,20 @@ let replacePatterns text =
     |> replace (@"^\s*\}[,;]?\n", @"")                              // replace "}[,;]" with ""
     |> replace (@"^( +)(.+?=>)", @"$1| $2")                         // replace "*=>" with "| *=>"
     |> replace (@"=>( \{)?( )?(.*?),?$", @"->$2$3")                 // replace "=>" with "->"
-    |> replace (@"::", @".")                                        // replace "::" with "."
+    |> replace (@"([^""])::", @"$1.")                               // replace "::" with "."
+    |> replace (@"([^""])==", @"$1=")                               // replace "==" with "="
+    |> replace (@"([^""])!=", @"$1<>")                              // replace "!=" with "<>"
 
-    |> replace (@" == ", @" = ")                                    // replace " == " with " = "
-    |> replace (@"([^\w])!([^\s=;,]+)", @"$1not($2)")               // replace "!" with "not"
-    |> replace (@"!\(", @"(")                                       // replace "!(" with "("
+    |> replace (@"([\s(])!([^\s=;,]+)", @"$1not($2)")               // replace "!" with "not"
+    |> replace (@"(\w)!\(", @"$1(")                                 // replace "!(" with "("
     |> replace (@"\)\?", @")")                                      // replace ")?" with ")"
     |> replace (@";$", @"")                                         // replace trailing ";" with ""
     |> replace (@";(\s*//)", @"$1")                                 // replace trailing "; //" with "//"
 
     |> replace (@"([^&""])&([^&""])", @"$1$2")                      // replace "&" with ""
-    |> replace (@"([^/""])[*]([^/""])", @"$1$2")                    // replace "*" with ""
-    |> replace (@"[/][*]", @"(*")                                   // replace "/*" with "(*"
-    |> replace (@"[*][/]", @"*)")                                   // replace "*/" with "*)"
+    |> replace (@"([^/""])[*](\w[^/""])", @"$1$2")                  // replace "*" with ""
+    |> replace (@"([^""])[/][*]", @"$1(*")                          // replace "/*" with "(*"
+    |> replace (@"[*][/]([^""])", @"*)$1")                          // replace "*/" with "*)"
 
     |> replace (@": \[([\w.]+?)\]", @": $1[]")                      // replace ": [T]" with ": T[]"
     |> replace (@"^( {8,}\w+): (.*[^=,]),?$", @"$1 = $2")           // replace ":" with "=" in records
@@ -92,3 +94,5 @@ let replacePatterns text =
     |> replace (@"\.to_string\(\)", @"")                            // replace ".to_string()" with ""
     |> replace (@"vec!\[(.*?)\]", @"Vec($1)")                       // replace "vec![]" with "Vec()"
     |> replace (@"\): Self\b", @")")                                // replace "): Self" with ")"
+    |> replace (@"(\s*\) )\{$", @"$1=\n")                           // replace ") {" with ") ="
+    |> replace (@"\[\.\.\]", @"")                                   // replace slices "[..]" with ""
